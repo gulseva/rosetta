c7f1d8c72ed968e5459df0a779866a6ace55e862
gulseva <alican@gulsevinlab.org>
2024-01-30 11:00:45 -0500

    Created a template for the unit testing with class type QueueTests

---
+++ source/src/apps/pilot/gulsevin/QueueTests.cxxtest.hh/QueueTests.cxxtest.hh
@@ -0,0 +1,57 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file  ././//s/r/c///a/p/p/s///p/i/l/o/t///g/u/l/s/e/v/i/n///Q/u/e/u/e/T/e/s/t/s/./c/x/x/t/e/s/t/./h/h/QueueTests.cxxtest.hh
+/// @brief  Unit test for the bootcamp test function
+/// @author gulseva (alican@gulsevinlab.org)
+
+
+// Test headers
+#include <test/UMoverTest.hh>
+#include <test/UTracer.hh>
+#include <cxxtest/TestSuite.h>
+#include <test/util/pose_funcs.hh>
+#include <test/core/init_util.hh>
+
+// Project Headers
+
+
+// Core Headers
+#include <core/pose/Pose.hh>
+#include <core/import_pose/import_pose.hh>
+
+// Utility, etc Headers
+#include <basic/Tracer.hh>
+
+static basic::Tracer TR("QueueTests");
+
+
+class QueueTests : public CxxTest::TestSuite {
+	//Define Variables
+
+public:
+
+	void setUp() {
+		core_init();
+
+	}
+
+	void tearDown() {
+
+	}
+
+
+
+	void test_first() {
+
+
+	}
+
+
+};
